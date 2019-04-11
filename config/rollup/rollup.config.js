a7d0952b4887b97b7237d9e12f9db6902662eb86
Luyin <myraous@gmail.com>
2019-04-11 21:27:20 +0900

    chore: add  rollup-plugin-serve

--- config/rollup/rollup.config.js
+++ config/rollup/rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
+import serve from 'rollup-plugin-serve';
  
 export default {
     input: './src/index.ts',
@@ -12,6 +13,12 @@ export default {
         typescript({
             tsconfig: (process.env.NODE_ENV === 'production') ? 
             './config/typescript/tsconfig.prod.json' : './config/typescript/tsconfig.dev.json'
+        }),
+        serve({
+            open: true,
+            contentBase: ['public'],
+            host: 'localhost',
+            port: 3000,
         })
     ]
 }
